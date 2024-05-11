8addad08feda4216429ae885dffe6819678f22f0
João Vítor Dores dos Santos <joaojocesantos@gmail.com>
2024-05-10 21:23:58 -0300

    v0.8.4.3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
     // Create a prepared statement with our query
-    const ps = context.env.test.prepare('SELECT * from users');
+    const ps = context.env.BANCO.prepare('SELECT * from Customers');
     const data = await ps.first();
 
     return Response.json(data);
