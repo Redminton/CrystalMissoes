03fd3a841e69818e58d83aae4aa3053393a870ec
João Vítor Dores dos Santos <joaojocesantos@gmail.com>
2024-05-23 19:27:03 -0300

    v0.3.8.6

--- _worker.js
+++ _worker.js
@@ -4495,7 +4495,7 @@ var teste = {
                 });
             }
             // TODO: Add your custom /api/* logic here.
-           // return new Response('Ok');
+           
         }
         // Otherwise, serve the static assets.
         // Without this, the Worker will error and no assets will be served.
