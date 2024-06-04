c466ab435645b631e6773e15ff1be465dfb3a8ad
João Vítor Dores dos Santos <joaojocesantos@gmail.com>
2024-06-04 06:41:31 -0300

    teste

--- _worker.js
+++ _worker.js
@@ -4512,7 +4512,7 @@ var x = {
                 }
                 const rows = result.rows;
                 // Converte os resultados da consulta em uma string HTML
-                let html = '<!DOCTYPE html><html lang="pt-br">< head ><meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0">';
+                let html = '<!DOCTYPE html><html lang="pt-br"><head><meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0">';
                 html += '<title>Produtos</title>';
                 html += '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity = "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin = "anonymous" ><link href="css/cssindex.css" rel="stylesheet"></head><body>';
                 html += '<div class="d-flex justify-content-center">';
