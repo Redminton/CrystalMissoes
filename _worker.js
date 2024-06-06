701824d05d5cf40d7e3704bd66441a35a5e534ce
João Vítor Dores dos Santos <joaojocesantos@gmail.com>
2024-06-06 20:16:10 -0300

    teste

--- _worker.js
+++ _worker.js
@@ -4477,54 +4477,86 @@ var x = {
                 }
                 const rows = result.rows;
                 // Converte os resultados da consulta em uma string HTML
-                let html = `<!DOCTYPE html><html lang="pt-br"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
-                                <title>Produtos</title>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"><link href="../css/cssindex.css" rel="stylesheet"></head><body>
-                <div class="container"><header class="p-3">
-                <div class="container">
-                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start"><a href="https://redminton.cloud" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                <div class="bi me-2" role="img" aria-label="Bootstrap">
-                <img src="../imagens/Crystal Missões (2).png" width="300rem" />
-                </div>
-                </a>
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="https://redminton.cloud" class="nav-link px-2 text-secondary">Início</a></li>
-                <li><a href="../produtos.html" class="nav-link px-2 text-white">Produtos</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">Sobre</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-                <li><a href="#contato" class="nav-link px-2 text-white">Contato</a></li>
-                </ul>
-                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                <input type="search" class="form-control form-control-dark text-start" placeholder="Pesquisar..." aria-label="Search"></form>
-                <div class="text-end">
-                <button onclick="window.location.href='login.html'" type="button" class="btn btn-warning">MinhaConta</button> </div></div>
+                let html = `<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="../css/cssindex.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <header class="p-3">
+            <div class="container">
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <ul class="nav nav-underline justify-content-center">
-                <li class="nav-item"><a class="nav-link text-white" href="https://redminton.cloud/produtos/">Colares</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="https://redminton.cloud/chaves/">Anéis</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="#">Brincos</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="#">Gargantilhas</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="#">Pulseiras</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="#">Conjuntos</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="#">Acessórios</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="#">Masculinos</a></li>
-                </ul></div></div></header>
-                                <div class="d-flex justify-content-center">`;
-                for (const row of rows) {
-                    html += `<div class="row"><div class="col-md-4"><div class="card m-3" style="width: 25%;">
-                <img src="../imagens/${row[6]}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h4 class="card-title">${row[1]}</h4>
-                    <h5>R$ ${row[4]} <span style="font-size: smaller; color: red;">10x de R$ ${row[4] / 10}</span></h5>
-                    <p id="menor" class="card-text">${row[5]}</p>
-                    <div class="text-center">
-                    <a href="produtoInfo.html?id=${row[0]}" class=" btn btn-primary">Mais Informações</a>
+                    <a href="https://redminton.cloud" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                        <div class="bi me-2" role="img" aria-label="Bootstrap">
+                            <img src="../imagens/Crystal Missões (2).png" width="300rem" />
+                        </div>
+                    </a>
+                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        <li><a href="https://redminton.cloud" class="nav-link px-2 text-secondary">Início</a></li>
+                        <li><a href="../produtos.html" class="nav-link px-2 text-white">Produtos</a></li>
+                        <li><a href="#" class="nav-link px-2 text-white">Sobre</a></li>
+                        <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+                        <li><a href="#contato" class="nav-link px-2 text-white">Contato</a></li>
+                    </ul>
+                    <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                        <input type="search" class="form-control form-control-dark text-start" placeholder="Pesquisar..." aria-label="Search">
+                    </form>
+                    <div class="text-end">
+                        <button onclick="window.location.href='login.html'" type="button" class="btn btn-warning">MinhaConta</button>
                     </div>
                 </div>
-            </div></div></div>`;
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <ul class="nav nav-underline justify-content-center">
+                        <li class="nav-item"><a class="nav-link text-white" href="https://redminton.cloud/produtos/">Colares</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="https://redminton.cloud/chaves/">Anéis</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#">Brincos</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#">Gargantilhas</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#">Pulseiras</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#">Conjuntos</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#">Acessórios</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="#">Masculinos</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <div class="d-flex justify-content-center">
+            <div class="row">
+`;
+
+                for (const row of rows) {
+                    html += `
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card m-3">
+                        <img src="../imagens/${row[6]}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h4 class="card-title">${row[1]}</h4>
+                            <h5>R$ ${row[4]} <span style="font-size: smaller; color: red;">10x de R$ ${(row[4] / 10).toFixed(2)}</span></h5>
+                            <p id="menor" class="card-text">${row[5]}</p>
+                            <div class="text-center">
+                                <a href="produtoInfo.html?id=${row[0]}" class="btn btn-primary">Mais Informações</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    `;
                 }
-                html += `</div>
-                 <script src="js/jquery.min.js"></script><script src = "js/bootstrap.min.js"></script ></body></html>`;
+
+                html += `
+            </div>
+        </div>
+    </div>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+`;
+
+               
 
                 return new Response(html, {
                     status: 200,
