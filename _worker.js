cd0f0d8e8580fb46539303726f45f07c61d8c685
João Vítor Dores dos Santos <joaojocesantos@gmail.com>
2024-06-24 07:21:46 -0300

    teste

--- _worker.js
+++ _worker.js
@@ -5245,43 +5245,8 @@ var sistema = {
         <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
 </div>
+<br>
 
-
-                                <div class="container" >
-                                    <table class="table table-striped table-bordered">
-                                        <thead class="thead-dark">
-                                            <tr>
-                                                <th>ID</th>
-                                                <th>Nome</th>
-                                                <th>Descrição</th>
-                                                <th>Categoria</th>
-                                                <th>Preço</th>
-                                                <th>Quantidade</th>
-                                                <th>Modificar</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            `;
-                            for (const row of rows) {
-                                acesso += `
-        <tr>
-            <td>${row[0]}</td>
-            <td>${row[1]}</td>
-            <td>${row[5]}</td>
-            <td>${row[3]}</td>
-            <td>${row[4]}</td>
-            <td>${row[2]}</td>
-            <td>
-                <a class="btn btn-primary btn-sm" href="/editppieastereggboanoite/${row[0]}">Editar</a>
-                <a class="btn btn-danger btn-sm" href="/deleteppieastereggboanoite/${row[0]}">Deletar</a>
-            </td>
-        </tr>
-    `;
-                            }
-                            acesso += `
-                                        </tbody>
-                                    </table>
-</div >
                             <br>
 
                                 <div class="container" >
