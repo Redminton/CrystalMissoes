75dc99dbce5fea1eccb8534c145a78d9fae9397f
João Vítor Dores dos Santos <joaojocesantos@gmail.com>
2024-06-24 00:35:01 -0300

    teste

--- _worker.js
+++ _worker.js
@@ -4921,7 +4921,76 @@ var sistema = {
                                 throw new Error('Unexpected result format');
                             }
                             const rows = result.rows;
-                            let acesso = `<!DOCTYPE html><html><head><title>CRUD</title></head><body>
+                            let acesso = `
+                            <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crystal Missões CRUD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="../css/cssindex.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <header class="p-3">
+            <div class="container">
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="https://redminton.cloud"
+                        class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                        <div class="bi me-2" role="img" aria-label="Bootstrap">
+                            <img src="../imagens/Crystal Missões (2).png" width="300rem" />
+                        </div>
+                    </a>
+                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        <li><a href="#" class="nav-link px-2 text-secondary">Início</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle px-2 text-white" id="produtosDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Produtos
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="produtosDropdown">
+                                <li><a class="dropdown-item"
+                                        href="https://redminton.cloud/mostrar/?cat=promo">Promoções</a>
+                                </li>
+                                <li><a class="dropdown-item"
+                                        href="https://redminton.cloud/mostrar/?cat=colar">Colares</a></li>
+                                <li><a class="dropdown-item" href="https://redminton.cloud/mostrar/?cat=anel">Anéis</a>
+                                </li>
+                                <li><a class="dropdown-item"
+                                        href="https://redminton.cloud/mostrar/?cat=brinco">Brincos</a></li>
+                                <li><a class="dropdown-item"
+                                        href="https://redminton.cloud/mostrar/?cat=promo">Gargantilhas</a>
+                                </li>
+                                <li><a class="dropdown-item"
+                                        href="https://redminton.cloud/mostrar/?cat=pulseira">Pulseiras</a>
+                                </li>
+                                <li><a class="dropdown-item"
+                                        href="https://redminton.cloud/mostrar/?cat=conjunto">Conjuntos</a>
+                                </li>
+                                <li><a class="dropdown-item"
+                                        href="https://redminton.cloud/mostrar/?cat=acessorio">Acessórios</a></li>
+                                <li><a class="dropdown-item"
+                                        href="https://redminton.cloud/mostrar/?cat=masculino">Masculinos</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="../about.html" class="nav-link px-2 text-white">Sobre</a></li>
+                        <li><a href="#contato" class="nav-link px-2 text-white">Contato</a></li>
+                    </ul>
+                    <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                        <input type="search" class="form-control form-control-dark text-start"
+                            placeholder="Pesquisar..." aria-label="Search">
+                    </form>
+
+                    <div class="text-end">
+                        <button onclick="window.location.href='../login.html'" type="button" class="btn btn-warning">Minha
+                            Conta</button>
+                    </div>
+                </div>
+            </div>
+        </header>
+                            <body>
                              <table border="1"><tr><th>IDProduto</th><th>Nome</th><th>Descricao</th><th>Categoria</th><th>Preco</th><th>Quantidade</th><th>Modificar</th></tr>`;
                             for (const row of rows) {
                                 acesso += ` <tr><td>${row[0]}</td><td>${row[1]}</td><td>${row[5]}</td><td>${row[3]}</td><td>${row[4]}</td><td>${row[2]}</td>
